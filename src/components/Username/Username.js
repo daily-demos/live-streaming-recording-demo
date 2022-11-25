2657f52d42b2e49c2e4be9e3bd588aeaa23ef388
Jayne Mast <mast.jayne@gmail.com>
2022-11-25 14:08:02 +0100

    Revert "Initial commit"
    This reverts commit e8905bc2ae78c701f9a1c348f4039aad92b67d26.

--- src/components/Username/Username.js
+++
@@ -1,8 +0,0 @@
-import './Username.css';
-import { useParticipantProperty } from '@daily-co/daily-react';
-
-export default function Username({ id }) {
-  const username = useParticipantProperty(id, 'user_name');
-
-  return <div className="username">{username || id}</div>;
-}
